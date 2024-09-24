a79e0e481c726e2cc3e2ec3d8869a2b0bbf1fe9a
sabinem <5292683+sabinem@users.noreply.github.com>
2024-09-24 11:21:08 +0200

    add config.js this file includes the references

---
+++ docs/releases/1.0/config.js
@@ -0,0 +1,145 @@
+var respecConfig = {
+  specStatus: "unofficial",
+  logos: [],
+  editors: [{
+    name: "ODTP",
+  }, {
+    name: "Jascha Grübel",
+    company:"Center for Sustainable Mobility",
+    companyURL:"https://csfm.ethz.ch/en/",
+  }, {
+    name:"Sabine Maennel",
+    company:"Swiss Data Science Center",
+    companyURL: "https://www.datascience.ch/",
+  }],
+  github: {
+    branch: "main",
+    repoURL: "https://github.com/odtp-org/odts",
+  },
+  localBiblio: {
+    "SDSC":{
+      "href": "https://www.datascience.ch/",
+      "title": "Swiss Data Science Center"
+    },
+    "CSFM":{
+      "href": "https://csfm.ethz.ch/",
+      "title": "ODTP SQL Dataloader",
+    },
+    "ODTP-Organization":{
+      "href": "https://github.com/odtp-org",
+      "title": "ODTP Organization",
+    },
+    "ODTP":{
+      "href": "https://github.com/odtp-org/odtp",
+      "title": "ODTP Reference Implementation of ODTWS",
+    },  
+    "BARFI":{
+      "href": "https://github.com/krish-adi/barfi",
+      "title": "BARFI",
+    },      
+    "Eqasim":{
+      "href": "https://eqasim.org/",
+      "title": "Eqasim",
+    },
+    "MATSim":{
+      "href": "MATSim: https://www.matsim.org/",
+      "title": "MATSim",
+    },                   
+    "ODTP-SQL-Dataloader":{
+      "href": "https://github.com/odtp-org/odtp-sql-dataloader",
+      "title": "ODTP SQL Dataloader",
+    },
+    "ODTP-Postgis-Dataloader":{
+      "href": "https://github.com/odtp-org/odtp-postgis-dataloader",
+      "title": "ODTP Postgis Dataloader",
+    },
+    "ODTP-Mobility-Simulation":{
+      "href": "https://github.com/odtp-org/odtp-mobility-simulation",
+      "title": "ODTP Mobility Simulation",
+    },
+    "ODTP-Mobility-Metrics":{
+      "href": "https://github.com/odtp-org/odtp-mobility-metrics",
+      "title": "ODTP Mobility Metrics",
+    }, 
+    "ODTP-Next-Location-Prediction":{
+      "href": "https://github.com/odtp-org/odtp-next-location-prediction",
+      "title": "ODTP Next Location Prediction",
+    },
+    "Odtp-Eqasim-Dataloader":{
+      "href": "https://github.com/odtp-org/odtp-eqasim-dataloader",
+      "title": "ODTP Eqasim Dataloader",
+    },
+    "ODTP-Eqasim":{
+      "href": "https://github.com/odtp-org/odtp-eqasim",
+      "title": "ODTP Eqasim",
+    },
+    "ODTP-Eqasim-Matsim":{
+      "href": "https://github.com/odtp-org/odtp-eqasim-matsim",
+      "title": "ODTP Eqasim Matsim",
+    },
+    "Odtp-Travel-Data-Dashboard":{
+      "href": "https://github.com/odtp-org/odtp-travel-data-dashboard",
+      "title": "ODTP Travel Data Dashboard",
+    },                
+    "Causal-interventions-in-Mobility":{
+      "href": "https://arxiv.org/abs/2311.11749",
+      "title": "Revealing behavioral impact on mobility prediction networks through causal interventions.",
+      "publisher": "Hong, Y., Xin, Y., Dirmeier, S., Perez-Cruz, F., & Raubal, M. (2023). arXiv preprint arXiv:2311.11749."
+    },
+    "Eqasim-in-Mobility":{
+      "href": "https://www.nsl.ethz.ch/en/eqasim-an-open-source-and-extensible-platform-for-building-agent-based-models/",
+      "title": "Eqasim: An open-source and extensible platform for building agent-based models.",
+      "publisher": "Hörl, S., & Balać, M. (2019). NSL Newsletter, 44."
+    },
+    "Odtp-Zoo-Frontend":{
+      "href": "https://odtp-org.github.io/odtp-zoo/",
+      "title": "ODTP Zoo Frontend",
+    },  
+    "Odtp-org-Zoo":{
+      "href": "https://github.com/odtp-org/odtp-zoo",
+      "title": "ODTP Zoo Github Repo",
+    },
+    "Odtp-Component-Template":{
+      "href": "https://github.com/odtp-org/odtp-component-template",
+      "title": "ODTP Component Template",
+    },
+    "Odtp-Component-Client":{
+      "href": "https://github.com/odtp-org/odtp-component-client",
+      "title": "ODTP Component Client",
+    },
+    "Odtp-Component-Example":{
+      "href": "https://github.com/odtp-org/odtp-component-example",
+      "title": "ODTP Component Example",
+    },
+    "Semantic-Versioning":{
+      "href": "https://semver.org/",
+      "title": "Semantic Versioning",
+      "publisher": "https://semver.org/"
+    },   
+    "Git":{
+      "href": "https://git-scm.com/",
+      "title": "Git (Version control)",
+    },  
+    "MongoDB":{
+      "href": "https://www.mongodb.com/",
+      "title": "MongoDB (Document Database)",
+    },  
+    "S3-Minio":{
+      "href": "https://min.io/",
+      "title": "S3 (Storage System)",
+    },  
+    "Docker":{
+      "href": "https://www.docker.com/",
+      "title": "Docker (Container Technology)",
+      "publisher": "https://www.docker.com/"
+    },  
+    "Keycloak":{
+      "href": "https://www.keycloak.org/",
+      "title": "Keycloak (Identity and Access Management)",
+    },    
+    "ISO":{
+      "href": "https://www.iso.org/foreword-supplementary-information.html",
+      "title": "ISO",
+    }                     
+  }   
+};
